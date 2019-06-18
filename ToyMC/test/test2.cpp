74a605bb38cf7aace874a34c3e1849516ed92315
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2019-06-18 15:56:51 +0200

    * improve test executable

--- ToyMC/test/test2.cpp
+++ ToyMC/test/test2.cpp
@@ -8,12 +8,15 @@
 #include "UVWprojector.h"
 #include "EventTPC.h"
 
+#include "TApplication.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TCanvas.h"
 #include "TBrowser.h"
 
 int main(int argc, char *argv[]) {
+
+  TApplication app("app", &argc, argv);
 
    ///Initialize projections
   std::string geomFileName = "resources/geometry_mini_eTPC.dat";
@@ -28,23 +31,26 @@ int main(int argc, char *argv[]) {
   aTree->GetEntry(0);
 
   aEvent->SetGeoPtr(myGeometryPtr);
-
+  
   double eventMaxCharge = aEvent->GetMaxCharge();
-  double chargeThreshold = 0.01*eventMaxCharge;
+  double chargeThreshold = 0.1*eventMaxCharge;
   int delta_timecells = 1;
   int delta_strips = 1;
-  double radius = 1.0;
+  double radius = 10.0;
   int rebin_space=EVENTTPC_DEFAULT_STRIP_REBIN;
   int rebin_time=EVENTTPC_DEFAULT_TIME_REBIN; 
   int method=EVENTTPC_DEFAULT_RECO_METHOD;
-    
+
+  aEvent->GetStripVsTime(DIR_U)->Draw("colz");
+  aEvent->GetStripVsTime(DIR_V)->Draw("colz");
+  aEvent->GetStripVsTime(DIR_W)->Draw("colz");
+
   SigClusterTPC aCluster = aEvent->GetOneCluster(chargeThreshold, delta_strips, delta_timecells);
   TH3F *aCluster_h3D = aEvent->Get3D(aCluster,  radius, rebin_space, rebin_time, method);
-
-  //TCanvas *c1 = new TCanvas("c1","c1", 900, 800);
-  //aCluster_h3D->Draw();
+  
   TBrowser* b = new TBrowser("clusterBrowser");
-  if(b && aCluster_h3D) std::cout<<"AAA"<<std::endl;
+  if(b && aCluster_h3D)   app.Run(kTRUE);
 
+ 
   return 0;
 }
