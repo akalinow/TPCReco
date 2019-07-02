5be748018a3121991d3fa3be5becf8be582da601
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2019-07-02 16:52:13 +0200

    * add a missing file

---
+++ Reconstruction/src/Track3D.cpp
@@ -0,0 +1,26 @@
+#include "Track3D.h"
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+Track3D::Track3D(const TVector3 & aT, const TVector3 & aB, double aLenght){
+  
+    myTangent = aT;
+    myTangentUnit = aT.Unit();
+    
+    myBias = aB;
+
+    double lambda = -myBias.X()/myTangent.X();
+    myBiasAtX0 = myBias + lambda*myTangent;
+
+    lambda = -myBias.Y()/myTangent.Y();
+    myBiasAtY0 = myBias + lambda*myTangent;
+
+    lambda = -myBias.Z()/myTangent.Z();
+    myBiasAtZ0 = myBias + lambda*myTangent;
+
+    myLenght = aLenght;
+}
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
