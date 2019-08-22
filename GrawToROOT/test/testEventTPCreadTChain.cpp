1d696fa8b15760398cffbc58c911ea2f75735dc5
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2019-08-22 09:12:44 +0200

    * update example programs for reading the data

--- GrawToROOT/test/testEventTPCreadTChain.cpp
+++ GrawToROOT/test/testEventTPCreadTChain.cpp
@@ -17,33 +17,41 @@
 int main(int argc, char *argv[]) {
 
   ///Initialize projections
-  std::string geomFileName = "resources/geometry_mini_eTPC.dat";
+  std::string geomFileName = "/home/akalinow/data/neutrons/ROOT/geometry_mini_eTPC_2018-06-19T11:18:25.056.dat";
   std::shared_ptr<GeometryTPC> myGeometryPtr = std::make_shared<GeometryTPC>(geomFileName.c_str());
   
-  std::string dataFileName = "resources/EventTPC_2018-06-19T11:18:25.056.root";
-  TFile aFile(dataFileName.c_str(),"READ");
-  TTree *aTree = (TTree*)aFile.Get("TPCData");
+  std::string dataFileName = "/home/akalinow/data/neutrons/ROOT/EventTPC_2018-06-19T11:18:25.056*root";
 
-  EventTPC *aEvent = 0;
+  ///Initialize chain of ROOT files.
+  TChain aChain("TPCData");
+  aChain.Add(dataFileName.c_str());
+  int nEntries = aChain.GetEntries();
+  std::cout<<"Total events in the chain: "<<nEntries<<std::endl;
 
+  ///Book histograms. Use smart pointers for automatic memory cleaning.
   std::shared_ptr<TH1D> hTotalCharge(new TH1D("hTotalCharge","Total charge;Charge[arb. units];Entries",100,0,500000));
   hTotalCharge->SetAxisRange(-100,1E8);//Draw under- and overflow bins
 
-  aTree->SetBranchAddress("Event", &aEvent);
+  ///Set the address of persistent object.
+  EventTPC *aEvent = 0;
+  aChain.SetBranchAddress("Event", &aEvent);
 
-  int nEntries = aTree->GetEntries();
-  std::cout<<"Entries in file: "<<dataFileName<<" "<<nEntries<<std::endl;
-  
+  ///Eneble multithreading (a simplest version).
+  int nthreads = 4;
+  ROOT::EnableImplicitMT(nthreads);
+
+  ///Loop over all events and fill histograms.
   for(int iEntry=0;iEntry<nEntries;++iEntry){
     if(iEntry%1000==0) std::cout<<"iEntry: "<<iEntry<<std::endl;
 
     aEvent->SetGeoPtr(0);    
-    aTree->GetEntry(iEntry);
+    aChain.GetEntry(iEntry);
     aEvent->SetGeoPtr(myGeometryPtr);
     
     hTotalCharge->Fill(aEvent->GetTotalCharge());
   }
 
+  ///Plot histograms.
   TCanvas *c1 = new TCanvas("c1","c1", 900, 800);
   hTotalCharge->Draw("hist");
   c1->Print("hTotalCharge.png");
