1f0e7c4880e18c0fa3b98c1f33125e5871734945
mfila <mfila@fuw.edu.pl>
2020-03-05 00:26:56 +0100

    very minor fix

--- GrawToROOT/src/EventSourceGRAW.cpp
+++ GrawToROOT/src/EventSourceGRAW.cpp
@@ -47,6 +47,7 @@ void EventSourceGRAW::loadFileEntry(unsigned long int iEntry){
     myCurrentEvent->SetGeoPtr(myGeometryPtr);
      std::cout<<"\033[34m";
      std::cout<<"Creating a new event: "<<eventIdx<<std::endl;
+     std::cout<<"Addng a frame from ASAD: "<< myDataFrame.fHeader.fAsadIdx<<std::endl;
     std::cout<<"\033[39m";
   }
   else{
@@ -63,8 +64,8 @@ void EventSourceGRAW::fillEventFromFrame(GET::GDataFrame & aGrawFrame){
 
   myPedestalCalculator.CalculateEventPedestals(aGrawFrame);
 
-  int  COBO_idx = myDataFrame.fHeader.fCoboIdx;
-  int  ASAD_idx = myDataFrame.fHeader.fAsadIdx;
+  int  COBO_idx = aGrawFrame.fHeader.fCoboIdx;
+  int  ASAD_idx = aGrawFrame.fHeader.fAsadIdx;
   if(ASAD_idx >= myGeometryPtr->GetAsadNboards()){
     std::cout<<"\033[31m";
     std::cout<<"Data format mismatch!. ASAD: "<<ASAD_idx
@@ -95,7 +96,7 @@ void EventSourceGRAW::fillEventFromFrame(GET::GDataFrame & aGrawFrame){
   //TProfile with pedestals is only 256 (max chans in frame) long, pedestals are calculated for each frame and reset
   //to fit into TProfile the global number of first chan in COBO/ASAD has to be substracted from global chanel
   int minChannelGlobal     = myGeometryPtr->Global_normal2normal(COBO_idx, ASAD_idx, 0, 0);
-	corrVal -= myPedestalCalculator.GetPedestalCorrection(iChannelGlobal-minChannelGlobal, agetId, icell);
+  corrVal -= myPedestalCalculator.GetPedestalCorrection(iChannelGlobal-minChannelGlobal, agetId, icell);
 //	} 
 	myCurrentEvent->AddValByAgetChannel(COBO_idx, ASAD_idx, agetId, chanId, icell, corrVal);
       }
