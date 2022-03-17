7d9b3e5d28c0e158c5d4ded5874fda69de8fe7dd
Mateusz Fila <mfila@fuw.edu.pl>
2022-03-17 18:15:09 +0100

    added MakeUniqueName #29

---
+++ Utilities/include/MakeUniqueName.h
@@ -0,0 +1,8 @@
+#ifndef UTILITIS_MAKE_UNIQUE_NAME_H_
+#define UTILITIS_MAKE_UNIQUE_NAME_H_
+
+#include <string>
+
+std::string MakeUniqueName(const std::string &filename);
+
+#endif //UTILITIS_MAKE_UNIQUE_NAME_H_
---
+++ Utilities/src/MakeUniqueName.cpp
@@ -0,0 +1,18 @@
+#include "MakeUniqueName.h"
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+std::string MakeUniqueName(const std::string &filename) {
+  auto path = fs::path(filename);
+  if (!fs::exists(path)) {
+    return filename;
+  }
+  size_t i = 0;
+  fs::path proposedPath = "";
+  do {
+    ++i;
+    auto proposedFilename = path.stem().string() + "(" + std::to_string(i) +
+                            ")" + path.extension().string();
+    proposedPath = path.parent_path() / fs::path(proposedFilename);
+  } while (fs::exists(proposedPath));
+  return proposedPath.string();
+}
