197c7d1986d947281bf2b43054e72977542bb6fc
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2022-03-21 11:47:14 +0100

    * fix Canvas name duplicate

--- GUI/src/MainFrame.cpp
+++ GUI/src/MainFrame.cpp
@@ -155,7 +155,7 @@ void MainFrame::InitializeEventSource(){
 
   if(myWorkMode!=M_ONLINE_MODE){
     myEventSource->loadDataFile(dataFileName);
-    myEventSource->loadFileEntry(296);//TEST
+    myEventSource->loadFileEntry(296);
   }
   myHistoManager.setGeometry(myEventSource->getGeometry());
   myHistoManager.openOutputStream(dataFileName);
@@ -210,7 +210,7 @@ void MainFrame::AddHistoCanvas(){
 
   
 
-  fRawHistosCanvas = new TCanvas("fRawHistosCanvas","Diagnostic Histograms",850,800);
+  fRawHistosCanvas = new TCanvas("fRawHistosCanvas","Raw Histograms",850,800);
   fRawHistosCanvas->MoveOpaque(kFALSE);
   fRawHistosCanvas->Divide(2,2, 0.02, 0.02);
   TList *aList = fRawHistosCanvas->GetListOfPrimitives();
