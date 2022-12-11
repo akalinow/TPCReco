72eb6a88584b580bf05396e682bbfadcb7b888fb
Mpasiowiec <mjp.pasiowiec@gmail.com>
2022-12-11 20:23:00 +0000

    continuation: changed test to partialy automatic

--- GrawToROOT/test/EventTPC_tst.cpp
+++ GrawToROOT/test/EventTPC_tst.cpp
@@ -13,7 +13,7 @@
 
 #include "colorText.h"
 
-void testHits(std::shared_ptr<EventTPC> aEventPtr, filter_type filterType, std::vector<double> comparision){
+int testHits(std::shared_ptr<EventTPC> aEventPtr, filter_type filterType, std::vector<double> comparision){
   
   std::cout<<KBLU<<"1D projection on strips: U, V, W [raw]"<<RST<<std::endl;
   std::cout<<aEventPtr->get1DProjection(projection_type::DIR_V, filterType, scale_type::raw)->GetTitle()<<std::endl;
@@ -63,56 +63,107 @@ void testHits(std::shared_ptr<EventTPC> aEventPtr, filter_type filterType, std::
     aEventPtr->GetChannels(0,0)->Print();
     aEventPtr->GetChannels_raw(0,0)->Print();
   }
+  std::vector<bool> error_list_bool;
+  std::vector<std::string> error_list = {"charge", "charge_DIR_U", "charge_DIR_U_strip1", "charge_DIR_U_sec1_strip_58", "charge_time_cell128", "charge_DIR_U_time_cell128", "charge_DIR_U_sec1_time_cell28", "max_charge", "max_charge_DIR_U", "max_charge_DIR_U_strip1", "max_charge_DIR_U_sec1_strip58", "max_charge_time", "max_charge_channel", "max_charge_time_DIR_U", "max_charge_strip_DIR_U", "min_time", "max_time", "min_time_DIR_U", "max_time_DIR_U", "min_strip_DIR_U", "max_strip_DIR_U", "multiplicity_total", "multiplicity_DIR_U", "multiplicity_DIR_V", "multiplicity_DIR_W", "multiplicity_DIR_U_0", "multiplicity_DIR_V_0", "multiplicity_DIR_W_0", "Nhits_total", "Nhits_DIR_U", "Nhits_DIR_V", "Nhits_DIR_W", "Nhits_DIR_U_0", "Nhits_DIR_U_0_70", "NhitsMerged_DIR_U_70"};
   std::cout << std::boolalpha;
   double epsilon = 1e-5;
-  bool charge = (aEventPtr->GetTotalCharge(-1, -1, -1, -1, filterType) - comparision[0]) < epsilon; std::cout << "charge: " << charge << std::endl;
-  bool charge_DIR_U = (aEventPtr->GetTotalCharge(DIR_U, -1, -1, -1, filterType) - comparision[1]) < epsilon; std::cout << "charge_DIR_U: " << charge_DIR_U << std::endl;
-  bool charge_DIR_U_strip1 = (aEventPtr->GetTotalCharge(DIR_U, -1, 1, -1, filterType) - comparision[2]) < epsilon; std::cout << "charge_DIR_U_strip1: " << charge_DIR_U_strip1 << std::endl;
-  bool charge_DIR_U_sec1_strip_58 = (aEventPtr->GetTotalCharge(DIR_U, 1, 58, -1, filterType) - comparision[3]) < epsilon; std::cout << "charge_DIR_U_sec1_strip_58: " << charge_DIR_U_sec1_strip_58 << std::endl;
-  bool charge_time_cell128 = (aEventPtr->GetTotalCharge(-1, -1, -1, 128, filterType) - comparision[4]) < epsilon; std::cout << "charge_time_cell128: " << charge_time_cell128 << std::endl;
-  bool charge_DIR_U_time_cell128 = (aEventPtr->GetTotalCharge(DIR_U, -1, -1, 128, filterType) - comparision[5]) < epsilon; std::cout << "charge_DIR_U_time_cell128: " << charge_DIR_U_time_cell128 << std::endl;
-  bool charge_DIR_U_sec1_time_cell28 = (aEventPtr->GetTotalCharge(DIR_U, 1,  -1, 128, filterType) - comparision[6]) < epsilon; std::cout << "charge_DIR_U_sec1_time_cell28: " << charge_DIR_U_sec1_time_cell28 << std::endl;
+  bool charge = (aEventPtr->GetTotalCharge(-1, -1, -1, -1, filterType) - comparision[0]) < epsilon;                             
+  bool charge_DIR_U = (aEventPtr->GetTotalCharge(DIR_U, -1, -1, -1, filterType) - comparision[1]) < epsilon;                    
+  bool charge_DIR_U_strip1 = (aEventPtr->GetTotalCharge(DIR_U, -1, 1, -1, filterType) - comparision[2]) < epsilon;              
+  bool charge_DIR_U_sec1_strip_58 = (aEventPtr->GetTotalCharge(DIR_U, 1, 58, -1, filterType) - comparision[3]) < epsilon;       
+  bool charge_time_cell128 = (aEventPtr->GetTotalCharge(-1, -1, -1, 128, filterType) - comparision[4]) < epsilon;               
+  bool charge_DIR_U_time_cell128 = (aEventPtr->GetTotalCharge(DIR_U, -1, -1, 128, filterType) - comparision[5]) < epsilon;      
+  bool charge_DIR_U_sec1_time_cell28 = (aEventPtr->GetTotalCharge(DIR_U, 1,  -1, 128, filterType) - comparision[6]) < epsilon;  
+  error_list_bool.push_back(charge);
+  error_list_bool.push_back(charge_DIR_U);
+  error_list_bool.push_back(charge_DIR_U_strip1);
+  error_list_bool.push_back(charge_DIR_U_sec1_strip_58);
+  error_list_bool.push_back(charge_time_cell128);
+  error_list_bool.push_back(charge_DIR_U_time_cell128);
+  error_list_bool.push_back(charge_DIR_U_sec1_time_cell28);
 
-  bool max_charge = (aEventPtr->GetMaxCharge(-1,-1,-1,filterType) - comparision[7]) < epsilon; std::cout << "max_charge: " << max_charge << std::endl;
-  bool max_charge_DIR_U = (aEventPtr->GetMaxCharge(DIR_U,-1,-1,filterType) - comparision[8]) < epsilon; std::cout << "max_charge_DIR_U: " << max_charge_DIR_U << std::endl;
-  bool max_charge_DIR_U_strip1 = (aEventPtr->GetMaxCharge(DIR_U, -1, 1, filterType) - comparision[9]) < epsilon; std::cout << "max_charge_DIR_U_strip1: " << max_charge_DIR_U_strip1 << std::endl;
-  bool max_charge_DIR_U_sec1_strip58 = (aEventPtr->GetMaxCharge(DIR_U, 1, 58,filterType) - comparision[10]) < epsilon; std::cout << "max_charge_DIR_U_sec1_strip58: " << max_charge_DIR_U_sec1_strip58 << std::endl;
+  bool max_charge = (aEventPtr->GetMaxCharge(-1,-1,-1,filterType) - comparision[7]) < epsilon;                          
+  bool max_charge_DIR_U = (aEventPtr->GetMaxCharge(DIR_U,-1,-1,filterType) - comparision[8]) < epsilon;                 
+  bool max_charge_DIR_U_strip1 = (aEventPtr->GetMaxCharge(DIR_U, -1, 1, filterType) - comparision[9]) < epsilon;        
+  bool max_charge_DIR_U_sec1_strip58 = (aEventPtr->GetMaxCharge(DIR_U, 1, 58,filterType) - comparision[10]) < epsilon;  
+  error_list_bool.push_back(max_charge);
+  error_list_bool.push_back(max_charge_DIR_U);
+  error_list_bool.push_back(max_charge_DIR_U_strip1);
+  error_list_bool.push_back(max_charge_DIR_U_sec1_strip58);
 
   int maxTime = 0, maxStrip = 0;
   std::tie(maxTime, maxStrip) = aEventPtr->GetMaxChargePos(-1,filterType);
-  bool max_charge_time = maxTime - comparision[11] == 0; std::cout << "max_charge_time: " << max_charge_time << std::endl;
-  bool max_charge_channel = 0 - comparision[12] == 0; std::cout << "max_charge_channel: " << max_charge_channel << std::endl;
+  bool max_charge_time = maxTime - comparision[11] == 0;  
+  bool max_charge_channel = 0 - comparision[12] == 0;     
+  error_list_bool.push_back(max_charge_time);
+  error_list_bool.push_back(max_charge_channel);
   std::tie(maxTime, maxStrip) = aEventPtr->GetMaxChargePos(DIR_U, filterType);
-  bool max_charge_time_DIR_U = maxTime - comparision[13] == 0; std::cout << "max_charge_time_DIR_U: " << max_charge_time_DIR_U << std::endl;
-  bool max_charge_strip_DIR_U = maxStrip - comparision[14] == 0; std::cout << "max_charge_strip_DIR_U: " << max_charge_strip_DIR_U << std::endl;
+  bool max_charge_time_DIR_U = maxTime - comparision[13] == 0;  
+  bool max_charge_strip_DIR_U = maxStrip - comparision[14] == 0;
+  error_list_bool.push_back(max_charge_time_DIR_U);
+  error_list_bool.push_back(max_charge_strip_DIR_U);
 
   int minTime=0, minStrip=0;
   std::tie(minTime, maxTime, minStrip, maxStrip) = aEventPtr->GetSignalRange(-1, filterType);
-  bool min_time = minTime - comparision[15] == 0; std::cout << "min_time: " << min_time << std::endl;
-  bool max_time = maxTime - comparision[16] == 0; std::cout << "max_time: " << max_time << std::endl;
+  bool min_time = minTime - comparision[15] == 0;
+  bool max_time = maxTime - comparision[16] == 0;
+  error_list_bool.push_back(min_time);
+  error_list_bool.push_back(max_time);
 
   std::tie(minTime, maxTime, minStrip, maxStrip) = aEventPtr->GetSignalRange(DIR_U, filterType);
-  bool min_time_DIR_U = minTime - comparision[17] == 0; std::cout << "min_time_DIR_U: " << min_time_DIR_U << std::endl;
-  bool max_time_DIR_U = maxTime - comparision[18] == 0; std::cout << "max_time_DIR_U: " << max_time_DIR_U << std::endl;
-  bool min_strip_DIR_U = minStrip - comparision[19] == 0; std::cout << "min_strip_DIR_U: " << min_strip_DIR_U << std::endl;
-  bool max_strip_DIR_U = maxStrip - comparision[20] == 0; std::cout << "max_strip_DIR_U: " << max_strip_DIR_U << std::endl;
+  bool min_time_DIR_U = minTime - comparision[17] == 0;    
+  bool max_time_DIR_U = maxTime - comparision[18] == 0;    
+  bool min_strip_DIR_U = minStrip - comparision[19] == 0;  
+  bool max_strip_DIR_U = maxStrip - comparision[20] == 0;
+  error_list_bool.push_back(min_time_DIR_U);
+  error_list_bool.push_back(max_time_DIR_U);
+  error_list_bool.push_back(min_strip_DIR_U);
+  error_list_bool.push_back(max_strip_DIR_U);
   
-  bool multiplicity_total = aEventPtr->GetMultiplicity(false, -1, -1, -1, filterType) - comparision[21] == 0; std::cout << "multiplicity_total: " << multiplicity_total << std::endl;
-  bool multiplicity_DIR_U = aEventPtr->GetMultiplicity(false, DIR_U, -1, -1, filterType) - comparision[22] == 0; std::cout << "multiplicity_DIR_U: " << multiplicity_DIR_U << std::endl;
-  bool multiplicity_DIR_V = aEventPtr->GetMultiplicity(false, DIR_V, -1, -1, filterType) - comparision[23] == 0; std::cout << "multiplicity_DIR_V: " << multiplicity_DIR_V << std::endl;
-  bool multiplicity_DIR_W = aEventPtr->GetMultiplicity(false, DIR_W, -1, -1, filterType) - comparision[24] == 0; std::cout << "multiplicity_DIR_W: " << multiplicity_DIR_W << std::endl;
+  bool multiplicity_total = aEventPtr->GetMultiplicity(false, -1, -1, -1, filterType) - comparision[21] == 0;     
+  bool multiplicity_DIR_U = aEventPtr->GetMultiplicity(false, DIR_U, -1, -1, filterType) - comparision[22] == 0;  
+  bool multiplicity_DIR_V = aEventPtr->GetMultiplicity(false, DIR_V, -1, -1, filterType) - comparision[23] == 0;  
+  bool multiplicity_DIR_W = aEventPtr->GetMultiplicity(false, DIR_W, -1, -1, filterType) - comparision[24] == 0;
+  error_list_bool.push_back(multiplicity_total);
+  error_list_bool.push_back(multiplicity_DIR_U);
+  error_list_bool.push_back(multiplicity_DIR_V);
+  error_list_bool.push_back(multiplicity_DIR_W);
   
-  bool multiplicity_DIR_U_0 = aEventPtr->GetMultiplicity(false, DIR_U, 0, -1, filterType) - comparision[25] == 0; std::cout << "multiplicity_DIR_U_0: " << multiplicity_DIR_U_0 << std::endl;
-  bool multiplicity_DIR_V_0 = aEventPtr->GetMultiplicity(false, DIR_V, 0, -1, filterType) - comparision[26] == 0; std::cout << "multiplicity_DIR_V_0: " << multiplicity_DIR_V_0 << std::endl;
-  bool multiplicity_DIR_W_0 = aEventPtr->GetMultiplicity(false, DIR_W, 0, -1, filterType) - comparision[27] == 0; std::cout << "multiplicity_DIR_W_0: " << multiplicity_DIR_W_0 << std::endl;
+  bool multiplicity_DIR_U_0 = aEventPtr->GetMultiplicity(false, DIR_U, 0, -1, filterType) - comparision[25] == 0;
+  bool multiplicity_DIR_V_0 = aEventPtr->GetMultiplicity(false, DIR_V, 0, -1, filterType) - comparision[26] == 0;
+  bool multiplicity_DIR_W_0 = aEventPtr->GetMultiplicity(false, DIR_W, 0, -1, filterType) - comparision[27] == 0;
+  error_list_bool.push_back(multiplicity_DIR_U_0);
+  error_list_bool.push_back(multiplicity_DIR_V_0);
+  error_list_bool.push_back(multiplicity_DIR_W_0);
 
-  bool Nhits_total = aEventPtr->GetMultiplicity(true, -1, -1, -1, filterType) - comparision[28] == 0; std::cout << "Nhits_total: " << Nhits_total << std::endl;
-  bool Nhits_DIR_U = aEventPtr->GetMultiplicity(true, DIR_U, -1, -1, filterType) - comparision[29] == 0; std::cout << "Nhits_DIR_U: " << Nhits_DIR_U << std::endl;
-  bool Nhits_DIR_V = aEventPtr->GetMultiplicity(true, DIR_V, -1, -1, filterType) - comparision[30] == 0; std::cout << "Nhits_DIR_V: " << Nhits_DIR_V << std::endl;
-  bool Nhits_DIR_W = aEventPtr->GetMultiplicity(true, DIR_W, -1, -1, filterType) - comparision[31] == 0; std::cout << "Nhits_DIR_W: " << Nhits_DIR_W << std::endl;
-  bool Nhits_DIR_U_0 = aEventPtr->GetMultiplicity(true, DIR_U, 0, -1, filterType) - comparision[32] == 0; std::cout << "Nhits_DIR_U_0: " << Nhits_DIR_U_0 << std::endl;
-  bool Nhits_DIR_U_0_70 = aEventPtr->GetMultiplicity(true, DIR_U, 0, 70, filterType) - comparision[33] == 0; std::cout << "Nhits_DIR_U_0_70: " << Nhits_DIR_U_0_70 << std::endl;
-  bool NhitsMerged_DIR_U_70 = aEventPtr->GetMultiplicity(true, DIR_U, -1, 70, filterType) - comparision[34] == 0; std::cout << "NhitsMerged_DIR_U_70: " << NhitsMerged_DIR_U_70 << std::endl;
+  bool Nhits_total = aEventPtr->GetMultiplicity(true, -1, -1, -1, filterType) - comparision[28] == 0;             
+  bool Nhits_DIR_U = aEventPtr->GetMultiplicity(true, DIR_U, -1, -1, filterType) - comparision[29] == 0;          
+  bool Nhits_DIR_V = aEventPtr->GetMultiplicity(true, DIR_V, -1, -1, filterType) - comparision[30] == 0;          
+  bool Nhits_DIR_W = aEventPtr->GetMultiplicity(true, DIR_W, -1, -1, filterType) - comparision[31] == 0;          
+  bool Nhits_DIR_U_0 = aEventPtr->GetMultiplicity(true, DIR_U, 0, -1, filterType) - comparision[32] == 0;         
+  bool Nhits_DIR_U_0_70 = aEventPtr->GetMultiplicity(true, DIR_U, 0, 70, filterType) - comparision[33] == 0;      
+  bool NhitsMerged_DIR_U_70 = aEventPtr->GetMultiplicity(true, DIR_U, -1, 70, filterType) - comparision[34] == 0; 
+  error_list_bool.push_back(Nhits_total);
+  error_list_bool.push_back(Nhits_DIR_U);
+  error_list_bool.push_back(Nhits_DIR_V);
+  error_list_bool.push_back(Nhits_DIR_W);
+  error_list_bool.push_back(Nhits_DIR_U_0);
+  error_list_bool.push_back(Nhits_DIR_U_0_70);
+  error_list_bool.push_back(NhitsMerged_DIR_U_70);
+
+  int check = error_list_bool.size();
+  for(std::vector<bool>::iterator it = error_list_bool.begin(); it != error_list_bool.end(); ++it)
+    check -= *it;
+  
+  if(check > 0){
+    for(std::vector<std::string>::size_type i = 0; i != error_list.size(); i++) {
+      if(error_list_bool[i]==0){
+        std::cout<<KRED<<error_list[i]<<RST<<std::endl;
+      }
+    }
+  }
+  
+  return check;
 }
 /////////////////////////////////////
 /////////////////////////////////////
@@ -126,28 +177,27 @@ int main(int argc, char *argv[]) {
   myEventSource->loadDataFile(dataFileName);
   std::cout << "File with " << myEventSource->numberOfEntries() << " frames opened." << std::endl;
 
-  std::vector<double> comparison_none = { 520098.542683 , 155908.768293 , 1407.329268293 , 367.042682927 , 2890.25609756 , 950.109756098 , -79.378 , 1735.09146341 , 1392.21341463 , 23.7256 , 22.2561 , 58 , 0 ,  58 , 65 ,
-                                        2 , 500 , 3 , 501 , 1 , 132 , 1018 , 131 , 224 , 225 , 0 , 74 , 74 , 290917 , 65868  , 112275  , 112774  , 0 , 0 , 499};
+  std::vector<double> comparison_none = { 520098.542683 , 155908.768293 , 1407.329268293 , 367.042682927 , 2890.25609756 , 950.109756098 , -79.378 , 1735.09146341 , 1392.21341463 , 23.7256 , 22.2561 , 58 , 0 ,  58 , 65 , 2 , 500 , 3 , 501 , 1 , 132 , 1018 , 131 , 224 , 225 , 0 , 74 , 74 , 290917 , 65868  , 112275  , 112774  , 0 , 0 , 499};
 
-  std::vector<double> comparison_threshold = { 794935.22561 , 226979.29878 , 0 , 0 , 3425.77439024 , 1032.90243902 , 0 , 1732.16463415 , 1385.27439024 , 0 , 0 , 58 , 0 , 58 , 65 , 33 , 251 , 37 , 250 , 46 , 81 , 112 ,
-                                             25 , 43 , 29 , 0 , 28 , 15 , 5874 , 1747 , 2266 , 1861 , 0 , 0 , 104};
-
+  std::vector<double> comparison_threshold = { 794935.22561 , 226979.29878 , 0 , 0 , 3425.77439024 , 1032.90243902 , 0 , 1732.16463415 , 1385.27439024 , 0 , 0 , 58 , 0 , 58 , 65 , 33 , 251 , 37 , 250 , 46 , 81 , 112 , 25 , 43 , 29 , 0 , 28 , 15 , 5874 , 1747 , 2266 , 1861 , 0 , 0 , 104};
+  int check=0;
   auto myEventPtr = myEventSource->getCurrentEvent();
   for(int i=89;i<90;++i){
     myEventSource->loadFileEntry(i);
   
     std::cout<<myEventPtr->GetEventInfo()<<std::endl;
-    testHits(myEventPtr, filter_type::none , comparison_none);
-    testHits(myEventPtr, filter_type::threshold , comparison_threshold);
+    check += testHits(myEventPtr, filter_type::none , comparison_none);
+    check += testHits(myEventPtr, filter_type::threshold , comparison_threshold);
   }
-
+  //std::cout<<"check"<<a+b<<std::endl;
   ///This part to be moved to GeometryTPC_tst.cpp
-  std::cout<<KBLU<<"Strip direction has reversed strip numbering wrt. cartesian coordinates: "<<RST<<std::endl;
-  std::cout<<KBLU<<"U: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_U)<<std::endl;
-  std::cout<<KBLU<<"V: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_V)<<std::endl;
-  std::cout<<KBLU<<"W: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_W)<<std::endl;
+  //std::cout<<KBLU<<"Strip direction has reversed strip numbering wrt. cartesian coordinates: "<<RST<<std::endl;
+  //std::cout<<KBLU<<"U: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_U)<<std::endl;
+  //std::cout<<KBLU<<"V: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_V)<<std::endl;
+  //std::cout<<KBLU<<"W: "<<RST<<myEventPtr->GetGeoPtr()->IsStripDirReversed(projection_type::DIR_W)<<std::endl;
   /////
-  
+
+  if(check>0){return -1;}
   return 0;
 }
 /////////////////////////////////////
