24232b4581523667c0d06b722e963558055c77f9
Piotr Podlaski <Piotr.Podlaski@fuw.edu.pl>
2023-01-17 18:03:36 +0100

    Added placeholder for EventGenerator class - MC generator to produce various events to be later simulated by the transport models (Geant, ToyMC)

---
+++ MonteCarlo/UtilsMC/include/EventGenerator.h
@@ -0,0 +1,15 @@
+//
+// Created by piotrek on 1/17/23.
+//
+
+#ifndef TPCSOFT_EVENTGENERATOR_H
+#define TPCSOFT_EVENTGENERATOR_H
+
+
+class EventGenerator
+{
+
+};
+
+
+#endif //TPCSOFT_EVENTGENERATOR_H
---
+++ MonteCarlo/UtilsMC/src/EventGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by piotrek on 1/17/23.
+//
+
+#include "EventGenerator.h"
