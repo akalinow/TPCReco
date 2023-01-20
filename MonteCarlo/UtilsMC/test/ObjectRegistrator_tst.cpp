498f290678f933493063b5e6cbe58638dad6bfcc
Piotr Podlaski <Piotr.Podlaski@fuw.edu.pl>
2023-01-20 17:31:36 +0100

    Add unit test for ObjectRegistrator

---
+++ MonteCarlo/UtilsMC/test/ObjectRegistrator_tst.cpp
@@ -0,0 +1,134 @@
+/**
+  \file
+  Test automatic registration in ObjectFacotry
+
+  \author Lukas Nellen
+  \version $Id$
+  \date 13 Mar 2004
+
+  Modified by Piotr.Podlaski@fuw.edu.pl
+  01.2023
+
+  \ingroup testing
+*/
+
+// header for unit test testObjectRegistrar
+
+#include <ObjectRegistrator.h>
+#include <ObjectFactory.h>
+#include "gtest/gtest.h"
+
+
+using namespace utl;
+
+
+namespace {
+
+    class InheritObjectBase {
+    public:
+        virtual ~InheritObjectBase() { }
+        virtual int GetId() = 0;
+    };
+
+    typedef ObjectFactory<InheritObjectBase*, int> InheritObjectFactory;
+
+
+    template<int id>
+    class InheritObject : public InheritObjectBase,
+                          private ObjectRegistrator<InheritObject<id>, InheritObjectFactory> {
+    public:
+        int GetId() { return GetRegistrationId(); }
+
+        static typename InheritObjectFactory::IdentifierType
+        GetRegistrationId()  { return id; }
+
+        static InheritObjectBase* Create()
+        { return new InheritObject; }
+    };
+
+    template class InheritObject<1>;
+    template class InheritObject<3>;
+
+
+    class MemberObjectBase {
+    public:
+        virtual ~MemberObjectBase() {}
+        virtual int GetId() = 0;
+    };
+
+    typedef ObjectFactory<MemberObjectBase*, int> MemberObjectFactory;
+
+
+    template<int id>
+    class MemberObject : public MemberObjectBase {
+    public:
+        int GetId() { return GetRegistrationId(); }
+
+        static typename MemberObjectFactory::IdentifierType
+        GetRegistrationId()  { return id; }
+
+        static MemberObjectBase* Create()
+        { return new MemberObject; }
+    private:
+        ObjectRegistrator<MemberObject<id>, MemberObjectFactory> fAutoReg;
+    };
+
+    template class MemberObject<1>;
+    template class MemberObject<3>;
+
+    class ExternalObjectBase
+    {
+    public:
+        virtual ~ExternalObjectBase() {}
+        virtual int GetId() = 0;
+    };
+
+    typedef utl::ObjectFactory<ExternalObjectBase*, int> ExternalObjectFactory;
+
+    template<int id>
+    class ExternalObject
+            : public ExternalObjectBase
+    {
+    public:
+        int GetId() { return GetRegistrationId(); }
+
+        static typename ExternalObjectFactory::IdentifierType
+        GetRegistrationId()  { return id; }
+
+        static ExternalObjectBase* Create()
+        { return new ExternalObject; }
+    private:
+        utl::ObjectRegistrator<ExternalObject<id>, ExternalObjectFactory> fAutoReg;
+    };
+
+
+}
+
+
+/**
+  \ingroup testing
+*/
+
+TEST(ObjectRegistrator, CreationInherit)
+{
+    ASSERT_EQ(InheritObjectFactory::GetNumberOfCreators(), 2u);
+
+    auto p = InheritObjectFactory::Create<InheritObjectBase>(1);
+    ASSERT_NE(p.get(), nullptr);
+}
+
+
+TEST(ObjectRegistrator, CreationMember)
+{
+    ASSERT_EQ(MemberObjectFactory::GetNumberOfCreators(), 2u);
+    auto p = MemberObjectFactory::Create<MemberObjectBase>(1);
+    ASSERT_NE(p.get(), nullptr);
+}
+
+void testCreationExternal()
+{
+    ASSERT_EQ(ExternalObjectFactory::GetNumberOfCreators(), 2u);
+
+    auto p = ExternalObjectFactory::Create<ExternalObjectBase>(1);
+    ASSERT_NE(p.get(), nullptr);
+}
