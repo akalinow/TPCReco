16351d9053bf9d9292ec9e6eea53986cf79c47f5
ag1234ata <a.bijak@student.uw.edu.pl>
2023-03-01 01:30:50 +0100

    Initial version of ConfigManager class

---
+++ Utilities/include/ConfigManager.h
@@ -0,0 +1,20 @@
+#include<map>
+#include<iostream>
+
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/program_options.hpp>
+
+class ConfigManager
+{
+    public:
+    ConfigManager();
+    boost::program_options::variables_map parseCmdLineArgs(int, char **);
+    void configureTree(boost::property_tree::ptree &, int, char**);//czy pracować na kopii czy oryginale?
+    /*użycie:
+    boost::property_tree::ptree tree;
+    configureTree(tree&);
+    */
+    private:
+
+};
