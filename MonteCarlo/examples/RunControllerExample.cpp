65b6371285add2f4ca1113a1940a8c336efd3919
Piotr Podlaski <Piotr.Podlaski@fuw.edu.pl>
2023-03-03 09:57:19 +0100

    Add plotting of the events - again, almost exact copy-paste of Mikolaj's code, but this time in a separate exec.

--- MonteCarlo/examples/RunControllerExample.cpp
+++ MonteCarlo/examples/RunControllerExample.cpp
@@ -1,6 +1,6 @@
 //#include "RunController.h"
 #include <boost/property_tree/json_parser.hpp>
-
+#include "TRandom.h"
 #include "RunController.h"
 //Interesting bug that I (PP) was not able to solve:
 //Without including at least one .h file with the usage of REGISTER_MODULE macro the registration does not work,
@@ -10,6 +10,16 @@
 namespace pt = boost::property_tree;
 
 int main(int argc, char **argv) {
+
+    if(argc<2){
+        std::cout<<"Usage: "<<argv[0]<<" <path to config file> <random seed (optional)>"<<std::endl;
+        return -1;
+    }
+    ULong64_t seed = 0;
+    if(argc>2){
+        seed = std::stoi(argv[2]);
+    }
+    gRandom->SetSeed(seed);
     fwk::RunController rc;
     pt::ptree topNode;
     pt::read_json(argv[1], topNode);
