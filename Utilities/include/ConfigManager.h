81652408e4564b68a8391ec34546b6898bc3df3d
ag1234ata <a.bijak@student.uw.edu.pl>
2023-03-14 01:38:46 +0100

    configureTree is void instead of ptree

--- Utilities/include/ConfigManager.h
+++ Utilities/include/ConfigManager.h
@@ -10,11 +10,7 @@ class ConfigManager
 {
     public:
     ConfigManager();
-    void configureTree(boost::property_tree::ptree, int, char**);//czy pracować na kopii czy oryginale?
-    /*użycie:
-    boost::property_tree::ptree tree;
-    boost::property_tree::ptree tree1 = configureTree(tree); 
-    */
+    void configureTree(boost::property_tree::ptree &, int, char**);
     void setEventType(boost::property_tree::ptree &tree, std::string evtype);
     std::string getEventType(boost::property_tree::ptree tree);
 
