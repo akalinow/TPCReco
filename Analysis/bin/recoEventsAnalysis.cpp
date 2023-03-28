ec8d46e8938330b4ed2f653a703260b5dfe14677
Mikolaj Cwiok <mikolaj.cwiok@fuw.edu.pl>
2023-03-29 00:46:05 +0200

    added command line options in recoEventAnalysis for: O-16 selection cuts, effective alpha/carbon length corrections, choosing between reconstructed event-by-event or fixed nominal photon energy for LAB-CMS boosts

--- Analysis/bin/recoEventsAnalysis.cpp
+++ Analysis/bin/recoEventsAnalysis.cpp
@@ -41,7 +41,16 @@ int analyzeRecoEvents(const  std::string & geometryFileName,
 		      const  double & beam_diameter, // beam spot diameter [mm] used by event quality cuts
 		      const  double & pressure, // [mbar]
 		      const  double & temperature, // [K]
-		      const  bool & makeTreeFlag);
+		      const  bool & makeTreeFlag,
+		      const  bool & nominalBoostFlag,
+		      const  double & alphaScaleCorr, // unitless
+		      const  double & alphaOffsetCorr, // [mm]
+		      const  double & carbonScaleCorr, // unitless
+		      const  double & carbonOffsetCorr, // [mm]
+		      const  double & alphaMinCut, // [mm]
+		      const  double & alphaMaxCut, // [mm]
+		      const  double & carbonMinCut, // [mm]
+		      const  double & carbonMaxCut); // [mm]
 
 std::istream& operator>>(std::istream& in, BeamDirection& direction){
   std::string token;
@@ -79,14 +88,23 @@ boost::program_options::variables_map parseCmdLineArgs(int argc, char **argv){
     ("help", "produce help message")
     ("geometryFile",  boost::program_options::value<std::string>()->required(), "string - path to the geometry file")
     ("dataFile",  boost::program_options::value<std::string>()->required(), "string - path to the RECO data file")
-    ("beamEnergy", boost::program_options::value<float>()->required(), "float - LAB gamma beam energy [MeV]")
-    ("beamDir", boost::program_options::value<BeamDirection>()->required(), "string - LAB gamma beam direction [\"x\" xor \"-x\"]")
-    ("beamOffset", boost::program_options::value<float>()->default_value(0.0), "float - LAB offset in Y_DET [mm] of actual beam line: Y_DET=slope*X_DET+offset, default=0 mm")
-    ("beamSlope", boost::program_options::value<float>()->default_value(0.0), "float - LAB slope (dY/dX)_DET of actual beam line: Y_DET=slope*X_DET+offset, default=0")
-    ("beamDiameter", boost::program_options::value<float>()->default_value(12.0), "float - LAB beam spot diameter [mm] used by event quality cuts, default=12 mm")
     ("pressure", boost::program_options::value<float>()->required(), "float - CO2 pressure [mbar]")
     ("temperature", boost::program_options::value<float>()->default_value(273.15+20), "(option) float - CO2 temperature [K], default=293.15K (20 C)")
-    ("noTree", boost::program_options::bool_switch()->default_value(false), "(option) bool - flag to skip creating additional TTree for 1,2,3-prongs, default=false ");
+    ("beamEnergy", boost::program_options::value<float>()->required(), "float - LAB gamma beam energy [MeV]")
+    ("beamDir", boost::program_options::value<BeamDirection>()->required(), "string - LAB gamma beam direction [\"x\" xor \"-x\"]")
+    ("beamOffset", boost::program_options::value<float>()->default_value(0.0), "(option) float - LAB offset in Y_DET [mm] of actual beam line: Y_DET=slope*X_DET+offset, default=0 mm")
+    ("beamSlope", boost::program_options::value<float>()->default_value(0.0), "(option) float - LAB slope (dY/dX)_DET of actual beam line: Y_DET=slope*X_DET+offset, default=0")
+    ("beamDiameter", boost::program_options::value<float>()->default_value(12.0), "(option) float - LAB beam spot diameter [mm] used by event quality cuts, default=12 mm")
+    ("alphaMinCut", boost::program_options::value<float>()->default_value(0.0), "(option) float - minimal allowed ALPHA track length [mm] for selecting 16-O candidates, default=0 mm")
+    ("alphaMaxCut", boost::program_options::value<float>()->default_value(300.0), "(option) float - maximal allowed ALPHA track length [mm] for selecting 16-O candidates, default=300 mm")
+    ("carbonMinCut", boost::program_options::value<float>()->default_value(0.0), "(option) float - minimal allowed C-12 track length [mm] for selecting 16-O candidates, default=0 mm")
+    ("carbonMaxCut", boost::program_options::value<float>()->default_value(300.0), "(option) float - maximal allowed C-12 track length [mm] for selecting 16-O candidates, default=300 mm")
+    ("alphaScaleCorr", boost::program_options::value<float>()->default_value(1.0), "(option) float - scaling factor for effective correction of ALPHA track length, default=1")
+    ("alphaOffsetCorr", boost::program_options::value<float>()->default_value(0.0), "(option) float - offset [mm] for effective correction of ALPHA track length, default=0 mm")
+    ("carbonScaleCorr", boost::program_options::value<float>()->default_value(1.0), "(option) float - scaling factor for effective correction of C-12 track length, default=1")
+    ("carbonOffsetCorr", boost::program_options::value<float>()->default_value(0.0), "(option) float - offset [mm] for effective correction of C-12 track lenght, default=0 mm")
+    ("noTree", boost::program_options::bool_switch()->default_value(false), "(option) bool - flag to skip creating additional TTree for 1,2,3-prongs, default=false ")
+    ("nominalBoost", boost::program_options::bool_switch()->default_value(false), "(option) bool - flag to use nominal beam energy for LAB<->CMS boost, default=false ");
   
   boost::program_options::variables_map varMap;  
 
@@ -117,12 +135,25 @@ int main(int argc, char **argv){
   auto pressure = varMap["pressure"].as<float>();
   auto temperature = varMap["temperature"].as<float>();
   auto makeTreeFlag = !varMap["noTree"].as<bool>();
+  auto nominalBoostFlag = varMap["nominalBoost"].as<bool>();
   auto beamOffset = varMap["beamOffset"].as<float>();
   auto beamSlope = varMap["beamSlope"].as<float>();
   auto beamDiameter = varMap["beamDiameter"].as<float>();
   auto beamDir = varMap["beamDir"].as<BeamDirection>();
+  auto alphaMinCut = varMap["alphaMinCut"].as<float>();
+  auto alphaMaxCut = varMap["alphaMaxCut"].as<float>();
+  auto carbonMinCut = varMap["carbonMinCut"].as<float>();
+  auto carbonMaxCut = varMap["carbonMaxCut"].as<float>();
+  auto alphaScaleCorr = varMap["alphaScaleCorr"].as<float>();
+  auto alphaOffsetCorr = varMap["alphaOffsetCorr"].as<float>();
+  auto carbonScaleCorr = varMap["carbonScaleCorr"].as<float>();
+  auto carbonOffsetCorr = varMap["carbonOffsetCorr"].as<float>();
 
-  analyzeRecoEvents(geometryFileName, dataFileName, beamEnergy, beamDir, beamOffset, beamSlope, beamDiameter, pressure, temperature, makeTreeFlag);
+  analyzeRecoEvents(geometryFileName, dataFileName, beamEnergy, beamDir, beamOffset, beamSlope, beamDiameter, pressure, temperature,
+		    makeTreeFlag, nominalBoostFlag,
+		    alphaScaleCorr, alphaOffsetCorr, carbonScaleCorr, carbonOffsetCorr,
+		    alphaMinCut, alphaMaxCut, carbonMinCut, carbonMaxCut);
+
   return 0;
 }
 /////////////////////////////
@@ -141,7 +172,16 @@ int analyzeRecoEvents(const  std::string & geometryFileName,
 		      const  double & beam_diameter, // beam spot diameter [mm] used by event quality cuts
 		      const  double & pressure, // [mbar]
 		      const  double & temperature, // [K]
-		      const  bool & makeTreeFlag){
+		      const  bool & makeTreeFlag,
+		      const  bool & nominalBoostFlag,
+		      const  double & alphaScaleCorr, // unitless
+		      const  double & alphaOffsetCorr, // [mm]
+		      const  double & carbonScaleCorr, // unitless
+		      const  double & carbonOffsetCorr, // [mm]
+		      const  double & alphaMinCut, // [mm]
+		      const  double & alphaMaxCut, // [mm]
+		      const  double & carbonMinCut, // [mm]
+		      const  double & carbonMaxCut){ // [mm]
 
   std::cout << __FUNCTION__ << ": Input parameters:" << std::endl
 	    << "* geometry file: " << geometryFileName << std::endl
@@ -149,7 +189,11 @@ int analyzeRecoEvents(const  std::string & geometryFileName,
 	    << "* nominal LAB gamma beam energy: " << beamEnergy << " MeV" << std::endl
     	    << "* nominal LAB gamma beam direction in DET coordinates: " << beamDir << std::endl
 	    << "* CO2 pressure (for ion ranges):  " << pressure <<" mbar" <<std::endl
-	    << "* CO2 temperature (for ion ranges): " << temperature << " K, "<< (temperature-273.15) << " C" << std::endl;
+	    << "* CO2 temperature (for ion ranges): " << temperature << " K, "<< (temperature-273.15) << " C" << std::endl
+	    << "* ALPHA length correction: scale="<<alphaScaleCorr<<" / offset="<<alphaOffsetCorr<<" mm"<<std::endl
+	    << "* C-12 length correction: scale="<<carbonScaleCorr<<" / offset="<<carbonOffsetCorr<<" mm"<<std::endl
+	    << "* O-16 identification cuts: ALPHA length=["<<alphaMinCut<<", "<<alphaMaxCut<<"] mm / C-12 length=["<<carbonMinCut<<", "<<carbonMaxCut<<"] mm"<<std::endl
+	    << "* use nominal LAB gamma beam energy for LAB<->CMS boost: "<<nominalBoostFlag<<std::endl;
 
   TFile *aFile = new TFile(dataFileName.c_str());
   if(!aFile || !aFile->IsOpen()){
@@ -183,10 +227,13 @@ int analyzeRecoEvents(const  std::string & geometryFileName,
 
   auto cuts = RequirementsCollection<std::function<bool(Track3D *)>>{};
   cuts.push_back(tpcreco::cuts::Cut1{});
-  cuts.push_back(tpcreco::cuts::Cut2{beam_offset, beam_slope, beam_diameter});
+  cuts.push_back(tpcreco::cuts::Cut2{beam_offset, beam_slope, fabs(beam_diameter)});
   cuts.push_back(tpcreco::cuts::Cut3{aGeometry.get(), 5});
   cuts.push_back(tpcreco::cuts::Cut4{aGeometry.get(), 25, 5});
-  cuts.push_back(tpcreco::cuts::Cut5{aGeometry.get(), beam_diameter});
+  cuts.push_back(tpcreco::cuts::Cut5{aGeometry.get(), beam_diameter}); // affects 2-prong only
+  cuts.push_back(tpcreco::cuts::Cut7{false, // affects 2-prong only
+	pid_type::ALPHA, std::min(alphaMinCut, alphaMaxCut), std::max(alphaMinCut, alphaMaxCut),
+	pid_type::CARBON_12, std::min(carbonMinCut, carbonMaxCut), std::max(carbonMinCut, carbonMaxCut)});
 
   // 1. Nominal transformation between DET-->BEAM coordinates for the HIGS experiment
   //    provided that beam direction is anti-paralell to X_DET (HIGS case, i.e. --beamDir="-X"):
@@ -241,7 +288,11 @@ int analyzeRecoEvents(const  std::string & geometryFileName,
   auto beamDir_DET = coordinateConverter.beamToDet({0,0,1}); // actual beam unit vector in DET coordinates (i.e. direction of photons)
   auto ionRangeCalculator = IonRangeCalculator(gas_mixture_type::CO2, pressure, temperature);
 
-  auto myAnalysis = HIGGS_analysis(aGeometry, beamEnergy, beamDir_DET, ionRangeCalculator, coordinateConverter);
+  // apply effective length corrections to ion range calculator
+  ionRangeCalculator.setEffectiveLengthCorrection(pid_type::ALPHA, alphaScaleCorr, alphaOffsetCorr);
+  ionRangeCalculator.setEffectiveLengthCorrection(pid_type::CARBON_12, carbonScaleCorr, carbonOffsetCorr);
+
+  auto myAnalysis = HIGGS_analysis(aGeometry, beamEnergy, beamDir_DET, ionRangeCalculator, coordinateConverter, nominalBoostFlag);
   auto myTreesAnalysis= std::unique_ptr<HIGS_trees_analysis>(nullptr);
   if(makeTreeFlag) myTreesAnalysis=std::make_unique<HIGS_trees_analysis>(aGeometry, beamEnergy, beamDir_DET, pressure, temperature);
 
