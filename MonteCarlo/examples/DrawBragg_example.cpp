4f9f312eabc36f7127f8d2bd6f0c731be5ae3fcf
Piotr Podlaski <Piotr.Podlaski@fuw.edu.pl>
2023-05-11 13:05:19 +0200

    refactor allocation of single-event bragg curve and formatting

--- MonteCarlo/examples/DrawBragg_example.cpp
+++ MonteCarlo/examples/DrawBragg_example.cpp
@@ -14,81 +14,84 @@
 #include "TPCReco/IonRangeCalculator.h"
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     gStyle->SetOptStat(0);
     gStyle->SetNumberContours(999);
-    int nEventsToDraw=0;
+    int nEventsToDraw = 0;
     std::string fname;
-    if(argc<2)
-    {
-        std::cout<<"Usage: "<<argv[0]<<" <filename> <number of events to draw> (optional)"<<std::endl;
-        std::cout<<"If <number of events to draw> is not provided all events in file will be drawn"<<std::endl;
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <filename> <number of events to draw> (optional)" << std::endl;
+        std::cout << "If <number of events to draw> is not provided all events in file will be drawn" << std::endl;
         return 1;
-    }
-    else if(argc>=2)
-    {
-        fname=argv[1];
+    } else if (argc >= 2) {
+        fname = argv[1];
     }
 
 
-    auto f=new TFile(fname.c_str());
+    auto f = new TFile(fname.c_str());
     TTree *t;
-    f->GetObject("TPCData",t);
-    if(argc>2)
-        nEventsToDraw=atoi(argv[2]);
+    f->GetObject("TPCData", t);
+    if (argc > 2)
+        nEventsToDraw = atoi(argv[2]);
     else
-        nEventsToDraw=t->GetEntries();
+        nEventsToDraw = t->GetEntries();
 
-    auto event=new SimEvent();
-    t->SetBranchAddress("SimEvent",&event);
-    int nEntries=t->GetEntries();
-    auto c=new TCanvas("c","",1024,768);
+    auto event = new SimEvent();
+    t->SetBranchAddress("SimEvent", &event);
+    int nEntries = t->GetEntries();
+    auto c = new TCanvas("c", "", 1024, 768);
     c->Print("bragg.pdf[");
-    double braggUpperRange=60;
+    double braggUpperRange = 60;
     double depXYrange = 10;
-    TH1D* hall=new TH1D("hall","A",200,0,braggUpperRange);
-    auto hXZ = new TH2D("hXZ","energy deposit, XZ-plane;x[mm];z[mm]",200,0,100,200,-depXYrange,depXYrange);
-    auto hYZ = new TH2D("hYZ","energy deposit, YZ-plane;y[mm];z[mm]",200,-depXYrange,depXYrange,200,-depXYrange,depXYrange);
-    auto hXY = new TH2D("hXY","energy deposit, XY-plane;x[mm];y[mm]",200,0,100,200,-depXYrange,depXYrange);
-    auto hRange = new TH1D("hRange","Range of particles;Range [mm]",200,40,braggUpperRange);
-    auto hScatDev = new TH1D("hScatDev","ange between momentum and displacement;arccos(#vec{d} #upoint #vec{p}/(|#vec{d}| #upoint |#vec{p}|)) [deg];",1000,0,30);
-    for(int i=0;i<t->GetEntries();i++)
-    {
-        if(i==nEntries)
+    TH1D *hall = new TH1D("hall", "A", 200, 0, braggUpperRange);
+    auto hXZ = new TH2D("hXZ", "energy deposit, XZ-plane;x[mm];z[mm]", 200, 0, 100, 200, -depXYrange, depXYrange);
+    auto hYZ = new TH2D("hYZ", "energy deposit, YZ-plane;y[mm];z[mm]", 200, -depXYrange, depXYrange, 200, -depXYrange,
+                        depXYrange);
+    auto hXY = new TH2D("hXY", "energy deposit, XY-plane;x[mm];y[mm]", 200, 0, 100, 200, -depXYrange, depXYrange);
+    auto hRange = new TH1D("hRange", "Range of particles;Range [mm]", 200, 40, braggUpperRange);
+    auto hScatDev = new TH1D("hScatDev",
+                             "ange between momentum and displacement;arccos(#vec{d} #upoint #vec{p}/(|#vec{d}| #upoint |#vec{p}|)) [deg];",
+                             1000, 0, 30);
+    for (int i = 0; i < t->GetEntries(); i++) {
+        if (i == nEntries)
             break;
         t->GetEntry(i);
-        for(const auto& track: event->GetTracks())
-        {
-            int a=track.GetPrimaryParticle().GetA();
-            int z=track.GetPrimaryParticle().GetZ();
-            double e=track.GetPrimaryParticle().GetKineticEnergy();
-            auto h=new TH1D("h",Form("E=%.2fMeV A=%d Z=%d event=%d",e,a,z,i),100,0,braggUpperRange);
-            auto start=track.GetStart();
+        for (const auto &track: event->GetTracks()) {
+            int a = track.GetPrimaryParticle().GetA();
+            int z = track.GetPrimaryParticle().GetZ();
+            double e = track.GetPrimaryParticle().GetKineticEnergy();
+
+            auto start = track.GetStart();
             hRange->Fill(track.GetRange());
-            for(const auto& hit : track.GetHits())
-            {
-                auto pos=hit.GetPosition();
-                auto d=pos-start;
-                auto eDep=hit.GetEnergy();
-                hXZ->Fill(pos.X(),pos.Z(),eDep);
-                hYZ->Fill(pos.Y(),pos.Z(),eDep);
-                hXY->Fill(pos.X(),pos.Y(),eDep);
-                if(i<nEventsToDraw) {
-                    h->Fill(d.Mag(),  eDep);
-                }
-                hall->Fill(d.Mag(),  eDep);
+            for (const auto &hit: track.GetHits()) {
+                auto pos = hit.GetPosition();
+                auto d = pos - start;
+                auto eDep = hit.GetEnergy();
+                hXZ->Fill(pos.X(), pos.Z(), eDep);
+                hYZ->Fill(pos.Y(), pos.Z(), eDep);
+                hXY->Fill(pos.X(), pos.Y(), eDep);
+                hall->Fill(d.Mag(), eDep);
             }
-            auto cos = (track.GetStop()-track.GetStart()).Dot(track.GetPrimaryParticle().GetMomentum().Unit())/(track.GetRange());
-            hScatDev->Fill(TMath::ACos(cos)*180/TMath::Pi());
-            if(i<nEventsToDraw) {
+            auto cos = (track.GetStop() - track.GetStart()).Dot(track.GetPrimaryParticle().GetMomentum().Unit()) /
+                       (track.GetRange());
+            hScatDev->Fill(TMath::ACos(cos) * 180 / TMath::Pi());
+            if (i < nEventsToDraw) {
+                // it is not optimal to have two loops over tracks, but now we avoid allocating
+                // TH1Ds on every event when it is not being drawn
+                auto h = std::make_unique<TH1D>("h", Form("E=%.2fMeV A=%d Z=%d event=%d", e, a, z, i), 100, 0,
+                                                braggUpperRange);
+                for (const auto &hit: track.GetHits()) {
+                    auto pos = hit.GetPosition();
+                    auto d = pos - start;
+                    auto eDep = hit.GetEnergy();
+                    h->Fill(d.Mag(), eDep);
+                }
                 h->Scale(1 / h->GetBinWidth(1));
                 h->GetXaxis()->SetTitle("Distance [mm]");
                 h->GetYaxis()->SetTitle("Energy deposit [MeV/mm]");
                 h->Draw("hist");
                 c->Print("bragg.pdf");
             }
-            delete h;
         }
 
         /*
@@ -116,26 +119,27 @@ int main(int argc, char** argv)
     }
 
     auto rCal = std::make_unique<IonRangeCalculator>();
-    auto g = rCal->getIonBraggCurveMeVPerMM(pid_type::ALPHA,3,1000);
+    auto g = rCal->getIonBraggCurveMeVPerMM(pid_type::ALPHA, 3, 1000);
 
     c->SetGridx();
     c->SetGridy();
 
-    hall->Scale(1./t->GetEntries());
-    std::cout<<"Total deposit: "<<hall->Integral()<<" "<<rCal->getIonBraggCurveIntegralMeV(pid_type::ALPHA, 3.0,1000)<<std::endl;
-    hall->Scale(1/hall->GetBinWidth(1));
+    hall->Scale(1. / t->GetEntries());
+    std::cout << "Total deposit: " << hall->Integral() << " "
+              << rCal->getIonBraggCurveIntegralMeV(pid_type::ALPHA, 3.0, 1000) << std::endl;
+    hall->Scale(1 / hall->GetBinWidth(1));
     hall->GetXaxis()->SetTitle("Distance [mm]");
     hall->GetYaxis()->SetTitle("Energy deposit [MeV/mm]");
-    hall->SetFillColorAlpha(kBlue-6,0.4);
+    hall->SetFillColorAlpha(kBlue - 6, 0.4);
     hall->SetTitle("E_{k} = 3 MeV, p = 250 mbar");
     hall->Draw("hist");
     g.SetLineColor(kRed);
     g.SetLineWidth(2);
     g.Draw("SAME L");
 
-    auto l = new TLegend(0.7,0.7,0.9,0.9);
-    l->AddEntry(&g,"SRIM","l");
-    l->AddEntry(hall,"Geant4","f");
+    auto l = new TLegend(0.7, 0.7, 0.9, 0.9);
+    l->AddEntry(&g, "SRIM", "l");
+    l->AddEntry(hall, "Geant4", "f");
     l->Draw();
 
 
