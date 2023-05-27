b4bcd532c02992e58f5287cfd44fabba9f04d3cd
Mikolaj Cwiok <mikolaj.cwiok@fuw.edu.pl>
2023-05-27 15:25:09 +0200

    added Beryllium-8 ion type definition

--- Utilities/include/TPCReco/CommonDefinitions.h
+++ Utilities/include/TPCReco/CommonDefinitions.h
@@ -41,28 +41,30 @@ definitions::projection_type get1DProjectionType(int aStrip_dir);
 definitions::projection_type get1DProjectionType(definitions::projection_type aStrip_dir);
 
 enum pid_type{
-  UNKNOWN=0,        //           <-- for backward compatibilty with data analyzed before 20 May 2022
-  ALPHA=1,          // Helium-4  <-- for backward compatibilty with data analyzed before 20 May 2022
-  CARBON_12=2,      // Carbon-12 <-- for backward compatibilty with data analyzed before 20 May 2022
-  CARBON_14=3,      // Carbon-14 <-- for backward compatibilty with data analyzed before 20 May 2022
-  C12_ALPHA=4,      //           <-- for backward compatibilty with data analyzed before 20 May 2022
-  PROTON,           // Hydrogen
-  CARBON_13,        // Carbon-13
-  NITROGEN_15,      // Nitrogen-15
-  OXYGEN_16,        // Oxygen-16
-  OXYGEN_17,        // Oxygen-17
-  OXYGEN_18,        // Oxygen-18
-  HELIUM_4=ALPHA,   // alias
-  C_12=CARBON_12,   // alias
-  C_13=CARBON_13,   // alias
-  C_14=CARBON_14,   // alias
-  N_15=NITROGEN_15, // alias
-  O_16=OXYGEN_16,   // alias
-  O_17=OXYGEN_17,   // alias
-  O_18=OXYGEN_18,   // alias
-  THREE_ALPHA,      //
-  PID_MIN=ALPHA,    // alias
-  PID_MAX=OXYGEN_18 // alias
+  UNKNOWN=0,          //           <-- for backward compatibilty with data analyzed before 20 May 2022
+  ALPHA=1,            // Helium-4  <-- for backward compatibilty with data analyzed before 20 May 2022
+  CARBON_12=2,        // Carbon-12 <-- for backward compatibilty with data analyzed before 20 May 2022
+  CARBON_14=3,        // Carbon-14 <-- for backward compatibilty with data analyzed before 20 May 2022
+  C12_ALPHA=4,        //           <-- for backward compatibilty with data analyzed before 20 May 2022
+  PROTON,             // Hydrogen
+  CARBON_13,          // Carbon-13
+  NITROGEN_15,        // Nitrogen-15
+  OXYGEN_16,          // Oxygen-16
+  OXYGEN_17,          // Oxygen-17
+  OXYGEN_18,          // Oxygen-18
+  BERYLLIUM_8,        // Beryllium-8
+  HELIUM_4=ALPHA,     // alias
+  C_12=CARBON_12,     // alias
+  C_13=CARBON_13,     // alias
+  C_14=CARBON_14,     // alias
+  N_15=NITROGEN_15,   // alias
+  O_16=OXYGEN_16,     // alias
+  O_17=OXYGEN_17,     // alias
+  O_18=OXYGEN_18,     // alias
+  BE_8=BERYLLIUM_8,   // alias
+  THREE_ALPHA,        //
+  PID_MIN=ALPHA,      // alias
+  PID_MAX=BERYLLIUM_8 // alias
 };
 
 enum gas_mixture_type{
