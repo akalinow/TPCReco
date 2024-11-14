427e27aa59292dbabff11d087a4416bd0c474b14
Mikolaj Cwiok <mikolaj.cwiok@fuw.edu.pl>
2024-11-14 22:00:30 +0100

    Optimized some plots

--- Analysis/examples/makePlotsRaw.cxx
+++ Analysis/examples/makePlotsRaw.cxx
@@ -3,6 +3,7 @@
 // root
 // root [0] .L makePlotsRaw.cxx
 // root [1] makePlotsRaw("RawSignalTree.root");
+// root [1] makePlotsRaw("RawSignalTree.root", true, 400, 300e3); // with max_charge (horiz.) and total_charge (vert.) markers
 //
 // Macro for looking at raw signals properties
 //
@@ -54,7 +55,8 @@ TTree *get_tree(TFile *tree_file, TString key){
 void set_global_style(){
   gStyle->SetHistLineWidth(2);
   //  gStyle->SetOptStat("neuoi");
-  gStyle->SetOptStat(0);
+  gStyle->SetOptStat("e"); // kisourmen
+  //  gStyle->SetOptStat(0);
   gStyle->SetPalette(kRainBow);
 }
 
@@ -89,7 +91,8 @@ void set_hist2D_style(TH2D* &hist, std::string xaxis, std::string yaxis, std::st
   }
 }
 
-int makePlotsRaw(std::string raw_tree_file_name){
+int makePlotsRaw(std::string raw_tree_file_name,
+		 bool marker_flag=false, float marker_maxCharge=400, float marker_totalCharge=300e3){
 
   TFile *raw_tree_file = open_TFile(raw_tree_file_name);
   TTree *raw_signal_tree = get_tree(raw_tree_file, "RawSignal");
@@ -98,30 +101,91 @@ int makePlotsRaw(std::string raw_tree_file_name){
   raw_tree_file->cd();
   raw_signal_tree->BuildIndex("runId", "eventId");
 
+  TFile *file = new TFile("Example_raw_signals.root", "RECREATE");
   TCanvas *window = new TCanvas("window", "", 800, 600);
   set_global_style();
   TH1D *totalCharge_all = new TH1D("totalCharge_all", "", 100,-10e5, 4e6);
   TH1D *maxCharge_all = new TH1D("maxCharge_all", "", 100, 0, 6000);
   TH2D *totalCharge_vs_maxCharge = new TH2D("totalCharge_vs_maxCharge", "", 100, -10e5, 4e6, 100, 0, 6e3);
-
+  TH2D *totalCharge_vs_maxCharge_CUT = new TH2D("totalCharge_vs_maxCharge_CUT", "", 100, -10e5, 4e6, 100, 0, 6e3);
+  TGraph *line = NULL;
   window->Print("Example_raw_signals.pdf[");
 
   raw_signal_tree->Draw("totalCharge>>totalCharge_all");
   set_hist_style(totalCharge_all, "Integrated charge [ADC units]", "Events/bin", kBlack, "Integrated charge distribution");
   totalCharge_all->Draw();
+  if(marker_flag) {
+    line=new TGraph();
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_all->GetMinimum());
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_all->GetMaximum());
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+  }
   window->Print("Example_raw_signals.pdf");
+  file->cd(); window->Write();
 
   raw_signal_tree->Draw("maxCharge>>maxCharge_all");
   set_hist_style(maxCharge_all, "Maximal charge [ADC units]", "Events/bin", kBlack, "Maximal charge distribution");
   maxCharge_all->Draw();
+  if(marker_flag) {
+    line=new TGraph();
+    line->SetPoint(line->GetN(), marker_maxCharge, maxCharge_all->GetMinimum());
+    line->SetPoint(line->GetN(), marker_maxCharge, maxCharge_all->GetMaximum());
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+  }
   window->Print("Example_raw_signals.pdf");
+  file->cd(); window->Write();
 
   raw_signal_tree->Draw("maxCharge:totalCharge>>totalCharge_vs_maxCharge");
   set_hist2D_style(totalCharge_vs_maxCharge, "Integrated charge [ADC units]", "Maximal charge [ADC units]", "Events/bin", 0, "Maximal charge vs Integrated charge");
   gPad->SetRightMargin(0.15);
   totalCharge_vs_maxCharge->Draw("colz");
+  if(marker_flag) {
+    line=new TGraph();
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_vs_maxCharge->GetYaxis()->GetXmin());
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_vs_maxCharge->GetYaxis()->GetXmax());
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+    line=new TGraph();
+    line->SetPoint(line->GetN(), totalCharge_vs_maxCharge->GetXaxis()->GetXmin(), marker_maxCharge); 
+    line->SetPoint(line->GetN(), totalCharge_vs_maxCharge->GetXaxis()->GetXmax(), marker_maxCharge);
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+  }
   window->Print("Example_raw_signals.pdf");
+  file->cd(); window->Write();
+
+  // same after zooming into upper-right quadrant
+  if(marker_flag) {
+    raw_signal_tree->Draw("maxCharge:totalCharge>>totalCharge_vs_maxCharge_CUT",Form("maxCharge>%le && totalCharge>%le", marker_maxCharge, marker_totalCharge));
+    set_hist2D_style(totalCharge_vs_maxCharge_CUT, "Integrated charge [ADC units]", "Maximal charge [ADC units]", "Events/bin", 0, "Maximal charge vs Integrated charge (with cut)");
+    gPad->SetRightMargin(0.15);
+    totalCharge_vs_maxCharge_CUT->Draw("colz");
+    line=new TGraph();
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_vs_maxCharge->GetYaxis()->GetXmin());
+    line->SetPoint(line->GetN(), marker_totalCharge, totalCharge_vs_maxCharge->GetYaxis()->GetXmax());
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+    line=new TGraph();
+    line->SetPoint(line->GetN(), totalCharge_vs_maxCharge->GetXaxis()->GetXmin(), marker_maxCharge); 
+    line->SetPoint(line->GetN(), totalCharge_vs_maxCharge->GetXaxis()->GetXmax(), marker_maxCharge);
+    line->SetLineColor(kMagenta);
+    line->SetLineStyle(kDashed);
+    line->Draw("L SAME");
+    //    totalCharge_vs_maxCharge_CUT->GetXaxis()->SetRangeUser(marker_totalCharge, totalCharge_vs_maxCharge->GetXaxis()->GetXmax());
+    //    totalCharge_vs_maxCharge_CUT->GetYaxis()->SetRangeUser(marker_maxCharge, totalCharge_vs_maxCharge->GetYaxis()->GetXmax());
+    //    totalCharge_vs_maxCharge_CUT->Draw("colz");
+  }
+  window->Print("Example_raw_signals.pdf");
+  file->cd(); window->Write();
 
   window->Print("Example_raw_signals.pdf]");
+  file->Close();
   return 0;
 }
